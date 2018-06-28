d59480f7efd7c7a11e6571c6e37b623f1d043015
Roberto Ferraz <roberto.ferrazn@gmail.com>
2018-06-28 17:03:54 +0200

    Updates gitignore

---
+++ Example/OCR test/Pods/Target Support Files/OCRSlicer/OCRSlicer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_OCRSlicer : NSObject
+@end
+@implementation PodsDummy_OCRSlicer
+@end
---
+++ Example/OCR test/Pods/Target Support Files/Pods-OCR test/Pods-OCR test-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OCR_test : NSObject
+@end
+@implementation PodsDummy_Pods_OCR_test
+@end
